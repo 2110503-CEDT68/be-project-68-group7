0247e17a2e9dd57e9a7e6dcc8d76e940d4a5e370
idiash18 <cream.kannawat@gmail.com>
2026-02-25 23:46:12 +0700

    Add index.js and config

---
+++ index.js
@@ -0,0 +1,21 @@
+const { setServers } = require("node:dns/promises");
+setServers(["1.1.1.1", "8.8.8.8"]);
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+connectDB();
+
+const app = express(); 
+
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 5000;
+
+const server =app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+process.on('unhandledRejection',(err,promise) => {
+    console.log(`Error: ${err.message}`);
+    server.close(()=>process.exit(1));
+})
