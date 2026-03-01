2b3579dd0ed9568f5a83c4f60a430fb206d8b31b
CosmicPun <41039@chs.ac.th>
2026-03-02 02:55:58 +0700

    add route and authentication

--- server.js
+++ server.js
@@ -1,16 +1,50 @@
 const { setServers } = require("node:dns/promises");
-setServers(["1.1.1.1", "8.8.8.8"]);
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-// Load env vars
-dotenv.config({ path: './config/config.env' });
-connectDB();
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('@exortek/express-mongo-sanitize');
+const helmet=require('helmet');
+const {xss}=require('express-xss-sanitizer');
+const rateLimit=require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+
+
+// Routes
+const authRoutes = require('./routes/auth');
+const providerRoutes = require('./routes/provider');
+const carRoutes = require('./routes/car');
+const bookingRoutes = require('./routes/booking');
+
+setServers(["1.1.1.1", "8.8.8.8"]);               // Set DNS servers
+dotenv.config({ path: './config/config.env' });   // Load env vars
+connectDB();                                      // Connect to database
+
+//Rate Limiting
+const limiter=rateLimit({
+    windowMs:10*60*1000, // 10 minutes
+    max: 100
+});
 
 const app = express(); 
 
-app.use(express.json());
+// Middleware
+app.set('query parser', 'extended');              // Parse query strings as objects
+app.use(express.json());                          // JSON Parsing
+app.use(cors());                                  // CORS for frontend requests
+app.use(helmet());                                // Security Headers 
+app.use(mongoSanitize());                         // Sanitize data
+app.use(xss());                                   // Prevent XSS attacks
+app.use(limiter);                                 // Rate Limiting
+app.use(hpp());                                   // Prevent parameter pollution
+app.use(cookieParser());                          // Cookie parser
 
+// Define routes
+app.use('/api/auth', authRoutes);
+app.use('/api/providers', providerRoutes);
+app.use('/api/cars', carRoutes);
+app.use('/api/bookings', bookingRoutes);
 
 const PORT = process.env.PORT || 5000;
 
