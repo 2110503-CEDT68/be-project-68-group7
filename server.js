3429b12d96069db970768e0e31d333992c6c6949
CosmicPun <41039@chs.ac.th>
2026-03-03 22:29:17 +0700

    increase limit sent

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ connectDB();                                      // Connect to database
 //Rate Limiting
 const limiter = rateLimit({
     windowMs: 10 * 60 * 1000, // 10 minutes
-    max: 100
+    max: 1000
 });
 
 const app = express();
